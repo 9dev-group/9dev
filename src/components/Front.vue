83048206ad0f6870b1624d16a310cf5897fd63d0
Seonghun Kang <lutis107@gmail.com>
2019-01-08 23:10:57 +0900

    Modify logo

--- src/components/Front.vue
+++ src/components/Front.vue
@@ -7,22 +7,29 @@
       align-center
     >
       <v-flex xs12 sm4 class="my-3">
-        <div class="text-xs-center">
-          <h1 class="display-4">{{ title }}</h1>
-        </div>
-        <img v-bind:src="image"/>
+        <img id="logo" v-bind:src="image"/>
       </v-flex>
     </v-layout>
   </section>
 </template>
 
 <script>
+const prodLogoImgPath = "https://raw.githubusercontent.com/greatfarmer/9dev/master/src/assets/images/logo/";
+const devLogoImgPath = "src/assets/images/logo/";
+
 export default {
   data() {
     return {
-      title: '9dev',
-      image: 'https://raw.githubusercontent.com/greatfarmer/9dev/master/src/assets/images/logo/valuto.jpg'
+      image: devLogoImgPath + 'Logo_01.png'
     }
   }
 }
 </script>
+
+<style scoped>
+img#logo {
+  width: 85%;
+  max-width: 500px;
+  height: auto;
+}
+</style>
